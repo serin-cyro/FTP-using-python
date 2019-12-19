df32947a13e3e39e5e00e3f4b3858e46ec59d44e
serin-cryo <53441435+serin-cryo@users.noreply.github.com>
2019-12-19 21:53:27 +0530

    Create client.py

---
+++ client.py
@@ -0,0 +1,22 @@
+from ftplib import FTP
+
+ftp = FTP('')
+ftp.connect('localhost',1026)
+ftp.login()
+ftp.cwd('directory_name') #replace with your directory
+ftp.retrlines('LIST')
+
+def uploadFile():
+ filename = 'testfile.txt' #replace with your file in your home folder
+ ftp.storbinary('STOR '+filename, open(filename, 'rb'))
+ ftp.quit()
+
+def downloadFile():
+ filename = 'testfile.txt' #replace with your file in the directory ('directory_name')
+ localfile = open(filename, 'wb')
+ ftp.retrbinary('RETR ' + filename, localfile.write, 1024)
+ ftp.quit()
+ localfile.close()
+
+uploadFile()
+#downloadFile()
