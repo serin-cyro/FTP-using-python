5564239142fdd16cf50845df5e29ffeabba5b3a2
serin-cryo <53441435+serin-cryo@users.noreply.github.com>
2019-12-19 21:59:47 +0530

    Create server.py

---
+++ server.py
@@ -0,0 +1,13 @@
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+authorizer = DummyAuthorizer()
+authorizer.add_user("user", "12345", "/home/username", perm="elradfmw")
+authorizer.add_anonymous("/home/username", perm="elradfmw")
+
+handler = FTPHandler
+handler.authorizer = authorizer
+
+server = FTPServer(("127.0.0.1", 1026), handler)
+server.serve_forever()
